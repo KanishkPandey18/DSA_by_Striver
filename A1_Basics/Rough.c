9f220afd282cfe127923b33f42f4795974d25d43
KanishkPandey18 <kanishkpandey240@gmail.com>
2026-01-29 22:58:58 +0530

    Basic LL done

--- A1_Basics/Rough.c
+++ A1_Basics/Rough.c
@@ -1,6 +1,22 @@
-#include<conio.h>
-#include<stdio.h>
-#define swap(a,b)temp=a;a=b;b=temp;
+#include <stdio.h>
 
-void main(){
+int main() {
+    int value = 42;         // 1. Declare an integer variable
+    int *ptr_to_value;      // 2. Declare an integer pointer
+
+    ptr_to_value = &value;  // 3. Store the address of 'value' in the pointer
+
+    // 4. Dereference the pointer to get the value
+    int dereferenced_value = *ptr_to_value;
+
+    printf("The value of 'value' is: %d\n", value);
+    printf("The address of 'value' is: %p\n", &value);
+    printf("The pointer 'ptr_to_value' holds the address: %p\n", ptr_to_value);
+    printf("The dereferenced value (using *ptr_to_value) is: %d\n", dereferenced_value);
+
+    // You can also use the dereference operator to modify the original value
+    *ptr_to_value = 99;
+    printf("The new value of 'value' is: %d\n", value);
+
+    return 0;
 }
--- A1_Basics/Rough.cpp
+++ A1_Basics/Rough.cpp
@@ -1,7 +1,16 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <iostream>
 
-int main(){
-    list<int> l({1,2,3,4,5,6});
-    cout << l.back();
+int main() {
+    int value = 10;
+    // Declare a pointer to an integer and store the address of 'value' in it
+    int* ptr = &value;
+
+    // Use the dereference operator (*) to access the data the pointer points to
+    int dereferenced_value = *ptr;
+
+    std::cout << "The value of the variable is: " << value << std::endl;
+    std::cout << "The address stored in the pointer is: " << ptr << std::endl;
+    std::cout << "The value at the address (dereferenced) is: " << dereferenced_value << std::endl;
+
+    return 0;
 }
